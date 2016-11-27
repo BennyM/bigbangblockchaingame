d5d1549324caa286a5b6b82cd70d91b5658e555f
Benny Michielsen <bennym@me.com>
2016-11-27 17:09:52 +0100

    Game files

---
+++ contracts/Game.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.4.0;
+contract Game {
+
+    enum State { None, Rock, Paper, Scissor, Lizard, Spock } 
+    
+    event Winner(address winner, address loser, State winnerState, State loserState); 
+    event Draw(State draw);
+
+    struct Hand{
+        State player1Hand;
+        State player2Hand;        
+    }
+
+    address public player1;
+    address public player2;
+    address public winner;
+    address gameLobbyaddr;
+    State public lastPlayedHand1;
+    State public lastPlayedHand2;
+    Hand[] public hands;
+
+    function Game(address player1addr, address player2addr){
+        gameLobbyaddr = msg.sender;
+        lastPlayedHand1 = State.None;
+        lastPlayedHand2 = State.None;
+        player1 = player1addr;
+        player2 = player2addr;
+    }
+
+    function playHand(State hand){        
+        if(winner != player1 && winner != player2)
+        {
+            if(msg.sender == player1 && lastPlayedHand1 == State.None)
+            {
+                lastPlayedHand1 = hand;
+            }
+            else if(msg.sender == player2 && lastPlayedHand2 == State.None)
+            {
+                lastPlayedHand2 = hand;
+            }
+            if(lastPlayedHand1 != State.None && lastPlayedHand2 != State.None)
+            {
+                declareWinner();
+            }
+        }        
+    }
+
+     function declareWinner(){
+        if(lastPlayedHand1 == lastPlayedHand2){
+            hands.push(Hand({player1Hand: lastPlayedHand1, player2Hand: lastPlayedHand2}));
+        } 
+        else
+        {          
+            State winningHand;
+            State losingHand;  
+            address loser;
+            if(lastPlayedHand1 == State.Rock && (lastPlayedHand2 == State.Lizard || lastPlayedHand2 == State.Scissor)){
+                winner = player1;
+                winningHand = lastPlayedHand1;
+                losingHand = lastPlayedHand2;
+                loser = player2;
+            } else if(lastPlayedHand1 == State.Paper && (lastPlayedHand2 == State.Rock || lastPlayedHand2 == State.Spock))
+            {
+                winner = player1;
+                winningHand = lastPlayedHand1;
+                losingHand = lastPlayedHand2;
+                loser = player2;
+            }
+            else if(lastPlayedHand1 == State.Scissor && (lastPlayedHand2 == State.Paper || lastPlayedHand2 == State.Lizard))
+            {
+                winner = player1;
+                winningHand = lastPlayedHand1;
+                losingHand = lastPlayedHand2;
+                loser = player2;
+            }
+            else if(lastPlayedHand1 == State.Lizard && (lastPlayedHand2 == State.Spock || lastPlayedHand2 == State.Paper))
+            {
+                winner = player1;
+                winningHand = lastPlayedHand1;
+                losingHand = lastPlayedHand2;
+                loser = player2;
+            }
+            else if(lastPlayedHand1 == State.Spock && (lastPlayedHand2 == State.Scissor || lastPlayedHand2 == State.Rock))
+            {
+                winner = player1;
+                winningHand = lastPlayedHand1;
+                losingHand = lastPlayedHand2;
+                loser = player2;
+            }
+            else
+            {
+                winner = player2;
+                winningHand = lastPlayedHand2;
+                losingHand = lastPlayedHand1;
+                loser = player1;
+            }
+            Winner(winner, loser, winningHand, losingHand);
+            GameLobby lobby = GameLobby(gameLobbyaddr);
+           lobby.gameEnded(winner);
+        }
+    }    
+}
---
+++ contracts/GameLobby.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.0;
+contract GameLobby{
+
+    address lobbyOwner;
+
+    address[] availablePlayers;
+    bool gameStarted;
+    address[] games;
+    mapping(address => int) public leaderboard;
+
+    event GameCreated(address game, address player1, address player2);
+
+    function GameLobby(){
+        lobbyOwner = msg.sender;
+        gameStarted = false;    
+    }
+
+    function openLobby(){
+        if(msg.sender == lobbyOwner){
+            gameStarted  = true;
+        }
+    }
+
+    function closeLobby(){
+        if(msg.sender == lobbyOwner){
+            gameStarted = false;
+        }
+    }
+
+    function signup(address availablePlayer){
+        if(gameStarted == true){
+            availablePlayers.push(availablePlayer);
+        }        
+    }
+
+    function startGame(address player1, address player2){
+         if(gameStarted == true){
+            address newGame = new Game(player1, player2);
+            games.push(newGame);
+            GameCreated(newGame, player1, player2);
+         }
+    }
+
+    function gameEnded(address winner){
+        for (uint index = 0;  index < games.length; index++) {
+            if(games[index] == msg.sender)
+            {
+                leaderboard[winner]++;
+            }
+        }
+    }
+
+
+
+}
