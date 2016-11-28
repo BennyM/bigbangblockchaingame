ceed54381e522796a41186dc8166d8bf6c25e6d7
Hans Peeters <hspeeters@gmail.com>
2016-11-28 23:21:41 +0100

    added angular

---
+++ web/BigBangBlockchainGame/js/app.js
@@ -0,0 +1,19 @@
+﻿(function() {
+
+    var app = angular.module('bigbangblockchain', ['ui.router']);
+
+    app.config(['$stateProvider', '$urlRouterProvider', '$compileProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $compileProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'js/home/home.html',
+                controller: 'HomeController'
+            })
+        ;
+
+        $urlRouterProvider.otherwise('/');
+    }]);
+
+})();
---
+++ web/BigBangBlockchainGame/js/home/homeController.js
@@ -0,0 +1,11 @@
+﻿(function() {
+
+    var app = angular.module('bigbangblockchain');
+
+    var homeController = function($scope) {
+        $scope.home = 'abc';
+    }
+
+    app.controller('HomeController', ['$scope', homeController]);
+
+})();
