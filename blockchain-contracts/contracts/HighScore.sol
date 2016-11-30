7b7a759665cc6dbf5d8279ee2d09636e4db9b111
Ubuntu <BennyM@EthereumTuffleDev.wp2voao2cicedk3ud0f2l4ir5b.ax.internal.cloudapp.net>
2016-11-30 22:09:02 +0000

    Added highscore

---
+++ blockchain-contracts/contracts/HighScore.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.0;
+
+contract HighScore {
+
+    mapping(address => uint) public leaderboard;
+    
+    address[] allowedGames;    
+    address owner;
+
+    event HighScoreChanged(address player, uint score);
+
+    function HighScore(address lobbyAddr){
+        owner = lobbyAddr;
+    }
+
+    function addAllowedGame(address gameAddr, address lobbyAddr){
+        if(lobbyAddr == owner){
+            allowedGames.push(gameAddr);
+        }
+    }
+
+    function increaseHighScore(address player, address gameAddr){
+        bool authorizedGame = false;
+        for (var index = 0; index < allowedGames.length; index++) {
+            if(allowedGames[index] == gameAddr){
+                authorizedGame = true;
+            }
+        }
+        if(authorizedGame)
+        {
+            leaderboard[player]++;
+            HighScoreChanged(player, leaderboard[player]);
+        }
+
+    }     
+        
+}
