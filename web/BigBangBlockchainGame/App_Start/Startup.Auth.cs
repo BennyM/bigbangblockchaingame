bb3a8de663e87f4eea47a0bd2abcd569f6b3e981
Glenn Dierckx <glenn@windstyle.be>
2016-11-30 23:22:26 +0100

    Fix auth

--- web/BigBangBlockchainGame/App_Start/Startup.Auth.cs
+++ web/BigBangBlockchainGame/App_Start/Startup.Auth.cs
@@ -7,7 +7,9 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using BigBangBlockchainGame.Models;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.Twitter;
 
 namespace BigBangBlockchainGame
 {
@@ -36,7 +38,7 @@ namespace BigBangBlockchainGame
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
@@ -52,9 +54,21 @@ namespace BigBangBlockchainGame
             //    clientId: "",
             //    clientSecret: "");
 
-            app.UseTwitterAuthentication(
-               consumerKey: ConfigurationManager.AppSettings["TwitterAppId"],
-               consumerSecret: ConfigurationManager.AppSettings["TwitterSecret"]);
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = ConfigurationManager.AppSettings["TwitterAppId"],
+                ConsumerSecret = ConfigurationManager.AppSettings["TwitterSecret"],
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                    "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
+                    "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+                })
+            });
+
 
 
             app.UseFacebookAuthentication(new FacebookAuthenticationOptions()
