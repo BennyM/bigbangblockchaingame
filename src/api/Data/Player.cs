3587922fcef31cdbab1c482405a01f1258a4e2c1
BennyM <bennym@me.com>
2017-05-31 10:18:35 +0200

    Init account with balance using confired account

--- src/api/Data/Player.cs
+++ src/api/Data/Player.cs
@@ -1,21 +1,23 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 
-namespace api.Data 
+namespace api.Data
 {
-    public class Player 
+    public class Player
     {
         public Guid Id { get; set; }
         public string Email { get; set; }
         public string Nickname { get; set; }
         public Guid LousySecurityKey { get; set; }
+
+        public string Address { get; set; }
     }
 
     public class BbbgContext : DbContext
     {
         public BbbgContext(DbContextOptions<BbbgContext> options)
             : base(options)
-        {            
+        {
         }
 
         public DbSet<Player> Players { get; set; }
