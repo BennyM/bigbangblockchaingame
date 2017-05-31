64ea5b9cd1c24648127d279ec354d5b477aef6a3
BennyM <bennym@me.com>
2017-05-31 12:06:46 +0200

    Deploy contract on blockchain

--- src/api/Controllers/GamesController.cs
+++ src/api/Controllers/GamesController.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Nethereum.Web3;
 
 namespace api.Data
 {
@@ -12,10 +15,11 @@ namespace api.Data
         : Controller
     {
         private BbbgContext _context;
-
-        public GamesController(BbbgContext context)
+        private IOptions<EthereumTransferAccount> _account;
+        public GamesController(BbbgContext context, IOptions<EthereumTransferAccount> account)
         {
             _context = context;
+            _account = account;
         }
 
         [HttpPost]
@@ -36,11 +40,25 @@ namespace api.Data
 
         [HttpPost]
         [Route("{id}/hand")]
-        public void PlayHand(long id, string hashedHand)
+        public async void PlayHand(long id, string hashedHand)
         {
             var opponentId = new Guid(User.Claims.Single(cl => cl.Type == ClaimTypes.NameIdentifier).Value);
             var game = _context.Games.Single(x => x.Id == id && x.OpponentId == opponentId  && x.OpponentHand == null);
             game.OpponentHand = hashedHand;
+            _context.SaveChanges();
+
+            var abi = "";
+            var byteCode ="";
+            var web3 = new Web3();
+            var deployedContractResult = await web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, _account.Value.Address, game.Challenger.Address, game.Opponent.Address, game.ChallengerHand, game.OpponentHand);
+            var receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(deployedContractResult);
+            while (receipt == null)
+            {
+                Thread.Sleep(5000);
+                receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(deployedContractResult);
+            }
+            var contractAddress = receipt.ContractAddress;
+            game.Address = contractAddress;
             _context.SaveChanges();
         }
     }
